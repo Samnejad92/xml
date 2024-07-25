0372852ce8f92c335ee568b2f8a30ba9b780f8cd
samnejad92 <m.samnejad92@gmail.com>
2024-07-25 23:18:56 +0330

    json-ajax

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ if (window.XMLHttpRequest) {
   request = new ActiveXObject('Microsoft.XMLHTTP')
 }
 
-request.open('GET', 'data.xml')
+request.open('GET', 'data.json')
 // request.open('GET', 'http://localhost:4046/api/values.asmx')
 request.onreadystatechange = function () {
   if (request.readyState === 4 && request.status === 200) {
-    console.log(request.responseXML.getElementsByTagName('name')[1].firstChild);
-    var items=request.responseXML.getElementsByTagName('name');
+    var items=JSON.parse(request.responseText);
+    console.log(items);
     var output = "<ul>";
 
-    for (let i = 0; i < items.length; i++) {
-      output+="<li>"+items[i].firstChild.nodeValue+"</li>";
+    for (key in items) {
+      output+="<li>"+items[key].name+" : "+items[key].bio+"</li>";
       
     }
 
